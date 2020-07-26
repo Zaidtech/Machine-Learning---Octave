fe82e293d70b15911135e1f59f906e10c475a6e0
Zaidtech <mzali@zhcet.ac.in>
2020-07-27 01:01:42 +0530

    Complete prediction

--- Multiclass-Classification&Neural-Network/ex3/predict.m
+++ Multiclass-Classification&Neural-Network/ex3/predict.m
@@ -10,7 +10,8 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 % p is m*1
-X = [ones(m,1), X];
+
+%X = [ones(m,1), X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -25,32 +26,32 @@ X = [ones(m,1), X];
 
 % a1 is the input layer 
 
-active = zeros(5000,10);
-for i = 1:m
-   z1 = Theta1*(X(i,:))';
-   a2 = sigmoid(z1);
-   a2 = [1;a2];
-   z2 = Theta2*a2;
-   a3 = sigmoid(sigmoid(z2));
-   active(i,:) = a3';
+%active = zeros(5000,10);
+
+ %  z1 = Theta1*(X(i,:))';
+ %  a2 = sigmoid(z1);
+ %  a2 = [1;a2];
+ %  z2 = Theta2*a2;
+ %  a3 = sigmoid(sigmoid(z2));
+ %  active(i,:) = a3';
    %[max_value, p] = max(H, [], 2);
-   endfor
+  % endfor
 
 % =========================================================================
- [max_value, p] = max(active, [], 2);
+ %[max_value, p] = max(active, [], 2);
 
 
 %=============================
 
 % OTHER METHOD
 
-%a1 = [ones(m,1), X];
-%a2 = [ones(m,1), sigmoid(a1 * Theta1')];
-%a3 = sigmoid(a2 * Theta2');
+a1 = [ones(m,1), X];
+a2 = [ones(m,1), sigmoid(a1 * Theta1')];
+a3 = sigmoid(a2 * Theta2');
 
-%h = sigmoid(a3);
+h = sigmoid(a3);
 
-%[max_value, p] = max(h, [], 2);
+[max_value, p] = max(h, [], 2);
 
 
 end
